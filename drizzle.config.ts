b2ee67437035957b0af90c8d89c60e3e285146fc
Nathan Shum <nathan.shum@berkeley.com>
2024-06-11 23:08:06 -0700

    connected drizzle and supabase

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+if (!process.env.DATABASE_URL) {
+  console.log('🚨 Cannot find database url');
+}
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/lib/supabase/schema.ts',
+  out: './migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || '',
+  },
+});
